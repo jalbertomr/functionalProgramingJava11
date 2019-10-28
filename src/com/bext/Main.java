d5755c39bd6734a4a7c83ad7e752718716a3d700
bext <jalbertomr@yahoo.com>
2019-10-28 14:47:03 -0600

    Generalizing Predicate with Function to receive a parameter

--- src/com/bext/Main.java
+++ src/com/bext/Main.java
@@ -2,6 +2,7 @@ package com.bext;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
@@ -13,14 +14,14 @@ public class Main {
         // find the double of the first even number greater than 3
         List<Integer> values = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
 
-        // Replacing static functions on main class with function into function
-        // with Predicate interface test
-        Predicate<Integer> isGreatherThan3 = number -> number > 3;
+        // Generalizing Predicate with Function to receive a parameter
+        //Predicate<Integer> isGreatherThan3 = number -> number > 3;
+        Function<Integer, Predicate<Integer>> isGreatherThan = pivot -> number -> number > pivot;
 
         System.out.println("with stream: " );
         final Stream<Integer> temp =
                 values.stream()
-                .filter(isGreatherThan3)
+                .filter(isGreatherThan.apply(3))
                 .filter( e-> isEven(e))
                 .map( e -> doubleIt(e));
         System.out.println(temp.findFirst());
