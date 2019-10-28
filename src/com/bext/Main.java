41e6c12d3184d2fb25225f1fd80caa38eb8a32a0
bext <jalbertomr@yahoo.com>
2019-10-28 13:48:49 -0600

    for vs stream

--- src/com/bext/Main.java
+++ src/com/bext/Main.java
@@ -1,5 +1,7 @@
 package com.bext;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
@@ -7,18 +9,25 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isPrime(1));
-        System.out.println(isPrime(2));
-        System.out.println(isPrime(3));
-        System.out.println(isPrime(4));
+        // find the double of the first even number greater than 3
+        List<Integer> values = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
+        int  result = 0;
+        for (int e : values ){
+            if ( e > 3 && e % 2 == 0) {
+                result = e * 2;
+                break;
+            }
+        }
+        System.out.println("with for : " + result);
+
+        System.out.println("with stream: " +
+                values.stream()
+                .filter(e -> e > 3)
+                .filter(e -> e % 2 == 0)
+                .map( e -> e * 2)
+                .findFirst()
+                .get()
+        );
     }
 
-    private static boolean isPrime(final int number) {
-        Predicate<Integer> isDivisible = divisor -> (number % divisor == 0);
-
-        return (number > 1) &&
-                IntStream.range(2, number)
-                        .noneMatch(index -> isDivisible.test(index));
-                        //java11 must be .test to call functional Interface.
-    }
 }
