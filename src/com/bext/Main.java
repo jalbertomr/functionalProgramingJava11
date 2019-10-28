bd04f63e055d2619869ae7cc44fbbeb67f2c43dd
bext <jalbertomr@yahoo.com>
2019-10-28 14:13:06 -0600

    Stream show Lazy optimization

--- src/com/bext/Main.java
+++ src/com/bext/Main.java
@@ -12,21 +12,31 @@ public class Main {
         // find the double of the first even number greater than 3
         List<Integer> values = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
 
+        // .filter High order funtion receives a function and decides if
+        // evaluate now, later or never.
+        // LAZY optimization
         System.out.println("with stream: " +
                 values.stream()
                 .filter(Main::isGreatherThan3)
                 .filter(Main::isEven)
-                .map( e -> e * 2)
+                .map(Main::doubleIt)
                 .findFirst()
         );
     }
 
     private static boolean isGreatherThan3(Integer number) {
+        System.out.println("isGreatherThan3 " + number);
         return number > 3;
     }
 
     private static boolean isEven(Integer number) {
+        System.out.println("isEven " + number);
         return number % 2 == 0;
+    }
+
+    private static int doubleIt(Integer number) {
+        System.out.println("doubleIt " + number);
+        return number * 2;
     }
 
 }
