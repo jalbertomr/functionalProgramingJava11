1d8ea5a22ccd24d49191d7b1c745bf1f463a1047
bext <jalbertomr@yahoo.com>
2019-10-28 15:14:42 -0600

    Dependency Inyection (Strategy Pattern) with Function composition, Predicate

--- src/com/bext/Main.java
+++ src/com/bext/Main.java
@@ -2,24 +2,14 @@ package com.bext;
 
 import java.util.Arrays;
 import java.util.List;
-
-interface Selector {
-    public boolean pick(int value);
-}
-
-class EvenSelector implements Selector {
-    @Override
-    public boolean pick(int value) {
-        return value % 2 == 0;
-    }
-}
+import java.util.function.Predicate;
 
 public class Main {
-    // Dependency Inyection (Strategy Pattern) with OOP composite
-    public static int totalValues(List<Integer> numbers, Selector selector) {
+    // Dependency Inyection (Strategy Pattern) with Function composition, Predicate
+    public static int totalValues(List<Integer> numbers, Predicate<Integer> selector) {
         int result = 0;
         for( int e : numbers) {
-            if (selector.pick(e)) result += e;
+            if (selector.test(e)) result += e;
         }
         return result;
     }
@@ -28,7 +18,7 @@ public class Main {
         List<Integer> values = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
 
         System.out.println("suma todos: " + totalValues(values, e -> true));
-        System.out.println("suma even: " + totalValues(values, new EvenSelector()));
+        System.out.println("suma even: " + totalValues(values, e -> e % 2 == 0));
     }
 
 
