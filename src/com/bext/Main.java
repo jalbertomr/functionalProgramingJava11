0c5a949804b6935b5c441a66b4ac0bd945a5470d
bext <jalbertomr@yahoo.com>
2019-10-28 15:11:58 -0600

    Dependency Inyection (Strategy Pattern) with OOP composite

--- src/com/bext/Main.java
+++ src/com/bext/Main.java
@@ -3,12 +3,23 @@ package com.bext;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+interface Selector {
+    public boolean pick(int value);
+}
 
-    public static int totalValues(List<Integer> numbers) {
+class EvenSelector implements Selector {
+    @Override
+    public boolean pick(int value) {
+        return value % 2 == 0;
+    }
+}
+
+public class Main {
+    // Dependency Inyection (Strategy Pattern) with OOP composite
+    public static int totalValues(List<Integer> numbers, Selector selector) {
         int result = 0;
         for( int e : numbers) {
-            result += e;
+            if (selector.pick(e)) result += e;
         }
         return result;
     }
@@ -16,7 +27,8 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> values = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
 
-        System.out.println(totalValues(values));
+        System.out.println("suma todos: " + totalValues(values, e -> true));
+        System.out.println("suma even: " + totalValues(values, new EvenSelector()));
     }
 
 
